669176be9b41b1e0a22a647fff10843e0769d053
Michael Owuor <michael.owuor@gmail.com>
2026-02-07 15:39:04 +0300

    chore: add terms-of-service to Vite build inputs

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
 			input: {
 				main: resolve(__dirname, "index.html"),
 				blogs: resolve(__dirname, "blogs.html"),
+				"terms-of-service": resolve(__dirname, "terms-of-service.html"),
 			},
 		},
 	},
